6d142feec0fe099491c29abee7120569b2a45170
Jagraj <jagraj3101@gmail.com>
2024-07-05 18:36:53 +0530

    04 - Modal Introduction

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./assets/styles.css";
+import { CustomModal } from "./components/CustomModal";
+import { DialogModal } from "./components/DialogModal";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isCustomModalOpen, setIsCustomModalOpen] = useState(false);
+  const [isDialogModalOpen, setIsDialogModalOpen] = useState(false);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+    <div>
+      <button onClick={() => setIsCustomModalOpen(true)}>
+        Show Custom Modal
+      </button>
+      <br />
+      <button onClick={() => setIsDialogModalOpen(true)}>
+        Show Dialog Modal
+      </button>
+      <CustomModal
+        isOpen={isCustomModalOpen}
+        onClose={() => setIsCustomModalOpen(false)}
+      >
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          This is a <strong>CUSTOM</strong> modal
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+        <button onClick={() => setIsCustomModalOpen(false)}>Close</button>
+      </CustomModal>
 
-export default App
+      <DialogModal
+        isOpen={isDialogModalOpen}
+        onClose={() => setIsDialogModalOpen(false)}
+      >
+        <p>
+          This is a <strong>DIALOG</strong> modal
+        </p>
+        <button onClick={() => setIsDialogModalOpen(false)}>Close</button>
+      </DialogModal>
+    </div>
+  );
+}
+export default App;
